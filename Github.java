4c9a445ffbc6e8f291c5b3bd2adf5037797b51d0
JayantHariharan <101041037+JayantHariharan@users.noreply.github.com>
2023-04-04 13:53:15 +0530

    initial commit

---
+++ Github.java
@@ -0,0 +1,16 @@
+
+
+public class Github {
+
+	public void games() {
+		System.out.println("God of War");
+		System.out.println("Assassin Creed");
+		System.out.println("Prince of persia");
+		System.out.println("GTA");
+		System.out.println("Saint Row");
+	}
+	public static void main(String args[]) {
+		Github git=new Github();
+		git.games();
+	}
+}
