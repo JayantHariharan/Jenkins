02d218ee7fc77f6b0707a10fbcc714e1eaab6643
JayantHariharan <101041037+JayantHariharan@users.noreply.github.com>
2023-04-04 14:09:27 +0530

    sleepingDogs updated

--- Github.java
+++ Github.java
@@ -8,8 +8,8 @@ public class Github {
 		System.out.println("Prince of persia");
 		System.out.println("GTA");
 		System.out.println("Saint Row");
-		System.out.println("Watch Dogs");//newly added1
-		System.out.println("Sleeping Dogs");
+		System.out.println("Watch Dogs");//newly added 1
+		System.out.println("Sleeping Dogs");//newly added 2
 	}
 	public static void main(String args[]) {
 		Github git=new Github();
